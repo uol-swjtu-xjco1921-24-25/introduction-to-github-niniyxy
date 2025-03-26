ad2ec1077cbda6f9ce8cfda33693e046ec3c5af0
niniyxy <125893623+niniyxy@users.noreply.github.com>
2025-03-26 13:27:38 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    long dp[1000]={0};
+    int i,j,n=3;
+    dp[0]=1;
+    for(i=1;i<n;i++){
+         for (j=n;j>=i;j--)
+            dp[j]=dp[j]+dp[j-i];
+    }
+    printf("%d",dp[n]);
+    return 0;
+}
